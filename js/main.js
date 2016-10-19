dcdbd1a1cb3aafc8082a3138dfb3c8b63d3d76c4
Okaidy <okaidy.german@gmail.com>
2016-10-19 11:58:25 -0500

    version final :tada:

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 function validateForm(){
+
+	console.log("Validar");
 	/* Escribe tú código aquí */
 	var nombre = document.getElementById("name");
 	var apellido = document.getElementById("lastname");
 	var correo = document.getElementById("input-email");
 	var contraseña = document.getElementById("input-password");
+	var opciones = document.getElementsByTagName('select')[0];
+	
+
+	var eliminados = document.getElementsByClassName('error');
+	console.log(eliminados);
+
+	while(eliminados.length > 0) {
+		var el = eliminados[0];
+		el.parentNode.removeChild(el);
+	}
 
 	var alertaNombre = document.createElement("span");
+	alertaNombre.setAttribute("class","error");
 	if(nombre.value.length<=0) {
 		alertaNombre.innerHTML="Debe de ingresar su nombre";
 		nombre.parentNode.appendChild(alertaNombre);
@@ -18,24 +31,30 @@ function validateForm(){
 	}
 
 	var alertaApellido = document.createElement("span");
+	alertaApellido.setAttribute("class","error");
 	if(apellido.value.length<=0) {
 		alertaApellido.innerHTML="Debe de ingresar su apellido";
 		apellido.parentNode.appendChild(alertaApellido);
 	}else if (!(/^[A-Z\s]*$/.test(apellido.value[0]))) {
-		alertaNombre.innerHTML="Primera letra de Nombre debe ser mayuscula";
-		nombre.parentNode.appendChild(alertaNombre);
+		alertaApellido.innerHTML="Primera letra de Nombre debe ser mayuscula";
+		apellido.parentNode.appendChild(alertaApellido);
 	}else if (!(/^[a-zA-Z\s]*$/.test(apellido.value))){
-		alertaNombre.innerHTML="El apellido no es valido";
-		nombre.parentNode.appendChild(alertaNombre);
+		alertaApellido.innerHTML="El apellido no es valido";
+		apellido.parentNode.appendChild(alertaApellido);
 	}
-
+	
+	var alertaCorreo = document.createElement("span");
+	alertaCorreo.setAttribute("class","error");
 	if(correo.value.length<=0) {
-		var alertaCorreo = document.createElement("span");
 		alertaCorreo.innerHTML="Debe de ingresar su correo";
+		correo.parentNode.appendChild(alertaCorreo);
+	} else if (!(/^([a-z]+[a-z1-9._-]*)@{1}([a-z1-9\.]{2,})\.([a-z]{2,3})$/).test(correo.value)) {
+		alertaCorreo.innerHTML="Correo no valido";
 		correo.parentNode.appendChild(alertaCorreo);
 	}
 	
 	var alertaPass = document.createElement("span");
+	alertaPass.setAttribute("class","error");
 	if(contraseña.value.length<=0) {
 		alertaPass.innerHTML="Debe de ingresar su contraseña";
 		contraseña.parentNode.appendChild(alertaPass);
@@ -46,5 +65,12 @@ function validateForm(){
 		alertaPass.innerHTML="La contraseña no es valida";
 		contraseña.parentNode.appendChild(alertaPass);
 	}
+
+	var alertaOpc = document.createElement("span");
+	alertaOpc.setAttribute("class","error");
+	if( opciones.selectedIndex == 0 ) {
+  		alertaOpc.innerHTML="Debes seleccionar al menos un tipo de bici";
+		opciones.parentNode.appendChild(alertaOpc);
+	}	
 
 }
